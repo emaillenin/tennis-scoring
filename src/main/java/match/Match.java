8859cc5bd92e90d734f401bcea2f03cb811759b4
Lenin <lerajasekaran@groupon.com>
2018-12-16 22:07:43 +1100

    Add Match class that holds scores & players

---
+++ src/main/java/match/Match.java
@@ -0,0 +1,25 @@
+package match;
+
+import scorer.GameScorer;
+import scorer.ScoreFormatter;
+import scores.GameScore;
+
+public class Match {
+    private final String player1;
+    private final String player2;
+    private GameScore score = new GameScore();
+    private GameScorer scorer = new GameScorer();
+
+    public Match(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void pointWonBy(String player) {
+        scorer.updateScore(score, player.equals(player1));
+    }
+
+    public String score() {
+        return new ScoreFormatter(score, player1, player2).formatScore();
+    }
+}
