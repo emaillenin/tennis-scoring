cb05512b9ea7405034ed5406c8fd40624756166b
Lenin <lerajasekaran@groupon.com>
2018-12-16 17:17:06 +1100

    Add GameScore to track points in a game

---
+++ src/main/java/scores/GameScore.java
@@ -0,0 +1,18 @@
+package scores;
+
+public class GameScore {
+    private Integer player1Score = 0; //try to use enum
+    private Integer player2Score = 0;
+
+    public void addPoints(String pointsWonBy) {
+
+    }
+
+    public Integer getPlayer1Score() {
+        return player1Score;
+    }
+
+    public Integer getPlayer2Score() {
+        return player2Score;
+    }
+}
