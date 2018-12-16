feff0476e2a6f8b30df543992b27975ec7b06c4c
Lenin <lerajasekaran@groupon.com>
2018-12-16 19:40:17 +1100

    Add logic to update scores and make tests pass

--- src/main/java/scorer/GameScorer.java
+++ src/main/java/scorer/GameScorer.java
@@ -5,6 +5,10 @@ import scores.GameScore;
 public class GameScorer {
 
     public void updateScore(GameScore score, Boolean pointByFirstPlayer) {
-
+        if (pointByFirstPlayer) {
+            score.scoreForPlayer1();
+        } else {
+            score.scoreForPlayer2();
+        }
     }
 }
