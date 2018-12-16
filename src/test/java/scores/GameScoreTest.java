ab2a4d8cd2074ccecefdbd82758534c233da12bc
Lenin <lerajasekaran@groupon.com>
2018-12-16 17:22:15 +1100

    Test for double points

--- src/test/java/scores/GameScoreTest.java
+++ src/test/java/scores/GameScoreTest.java
@@ -12,4 +12,12 @@ public class GameScoreTest {
         gameScore.scoreForPlayer1();
         assertEquals((Integer) 15, gameScore.getPlayer1Score());
     }
+
+    @Test
+    public void addDoublePoints() {
+        GameScore gameScore = new GameScore();
+        gameScore.scoreForPlayer2();
+        gameScore.scoreForPlayer2();
+        assertEquals((Integer) 30, gameScore.getPlayer2Score());
+    }
 }
